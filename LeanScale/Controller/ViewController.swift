77e74165e3e2a7b9703d81af620fc9579ade7cba
Amir Daliri <amir.daliri@jaaimsapp.com>
2020-06-20 00:20:26 +0300

    initial

---
+++ LeanScale/Controller/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  LeanScale
+//
+//  Created by Amir Daliri on 6/19/20.
+//  Copyright © 2020 Amir Daliri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Lifecycle Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // I'm Here...
+    }
+
+
+}
+
